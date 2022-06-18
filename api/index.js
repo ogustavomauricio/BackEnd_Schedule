29d329aea0bbd76b99ec7696f6ed949e63603f10
ogustavomauricio <gmauricio207@gmail.com>
2022-06-18 14:20:18 -0300

    comenta variavel port

--- api/index.js
+++ api/index.js
@@ -10,19 +10,6 @@ const port = process.env.PORT || 3001
 
 routes(app)
 
-// app.post('/cadastro', async (req, res) => {
-//   //   console.log(req.body);
-//   //   // res.send('pagina professor')
-//   //   try {
-//   //   //   const { name, email, password } = req.body;     
-//   //     const newUser =  await db.Professor.create(req.body);  
-//   //     return res.status(201).json(newUser);
-//   //   } catch (e) {
-//   //     console.log(e.message);
-//   //     res.status(500).json({ message: 'Algo deu errado' });
-//   //   }
-//   // });
-
 app.listen(port, () => console.log(`SERVIDOR RODANDO NA PORTA ${port}`));
 
 module.exports = app;
